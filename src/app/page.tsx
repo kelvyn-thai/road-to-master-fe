033742ac9b65a1000739b6c87e6ebff050ce4e60
jayce.thai <phat.tnh@carehealth.io>
2025-03-17 15:21:01 +0700

    Add faker and uuid packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      <h1>Helcome to our Homepage</h1>
+      <Link href="/about">About</Link>
+      <Link href="/posts/1">Post 1</Link>
+      <Link href="/posts/2">Post 2</Link>
     </main>
   );
 }
