f7396e8ac01af64f801e3f2fa20f12660a854980
jayce.thai <phat.tnh@carehealth.io>
2025-03-18 10:14:27 +0700

    Set up Jest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Layout } from "@/components/layout";
-import { redisConfig } from "@/config/redis.config";
 
-export default function Home() {
-  console.info({ redisConfig });
+export default function Page() {
   return (
     <Layout>
-      <h1 className="text-3xl leading-8 font-bold mb-4">
+      <h1
+        className="text-3xl leading-8 font-bold mb-4"
+        data-testid="welcome-msg"
+      >
         Welcome to our Homepage
       </h1>
       {process.env.NEXT_PUBLIC_API_KEY}
