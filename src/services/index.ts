7dffb351cad9a3de997018f8a9a64a3e72bc2ece
jayce.thai <phat.tnh@carehealth.io>
2025-04-05 17:27:01 +0700

    Fix issue casing

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,7 @@
 export * from "./authenticate.service";
-export * from "./github.service";
+export * from "./user.service";
 export * from "./project.service";
 
 export { default as AuthServices } from "./authenticate.service";
-export { default as GithubService } from "./github.service";
+export { default as GithubService } from "./user.service";
 export { default as ProjectService } from "./project.service";
